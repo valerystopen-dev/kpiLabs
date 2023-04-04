73c07d04498ca6e8ec86f115bfb767b70e6365d2
valerystopen-dev <stopen.lera.lg@gmail.com>
2023-04-04 19:24:52 +0300

    part 2 lab 1 data search finish

--- src/DataSearch/Lab1/Lab12.js
+++ src/DataSearch/Lab1/Lab12.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import './lab1.css'
 
 export const Lab12 = () => {
@@ -6,7 +6,99 @@ export const Lab12 = () => {
     const [counter, setCounter] = useState(1);
     const [documentCollections, setDocumentCollections] = useState([])
     const [searchQuery, setSearchQuery] = useState([])
+    const [threshold, setThreshold] = useState(0.1)
+    const [searchResult, setSearchResult] = useState([])
 
+    useEffect(()=>{
+        if(searchQuery.length>0){
+            performSearch()
+        }
+    }, [searchQuery])
+
+    const cosSimilarity = (vecA, vecB) => {
+        let dotProduct = 0
+        vecA.forEach((el, index) => {
+            dotProduct += el * vecB[index]
+        })
+        let magA = 0
+        vecA.forEach((el) => {
+            magA += el * el
+        })
+        magA = Math.sqrt(magA);
+        let magB = 0;
+        vecB.forEach((el) => {
+            magB += el * el
+        })
+        magB = Math.sqrt(magB);
+
+        return dotProduct / (magA * magB);
+    }
+
+    const getDenominator = (term, documents) => {
+        return Math.max(
+            ...documents.map((el) => {
+                return getNumerator(term, el);
+            })
+        );
+    };
+
+    const getNumerator = (term, documents) => {
+        return documents.filter((el) => el === term).length;
+    };
+
+    const performSearch = () => {
+        const result = [];
+
+        const documentsTerms = documentCollections.map((doc) => {
+            return doc.toLowerCase().split(/[ ,.]+/);
+        })
+        const inputQuery = searchQuery.toLowerCase();
+        const searchTerms = inputQuery.split(/[ ,.]+/);
+        const words = new Set([
+            ...documentsTerms.flat(Infinity),
+            ...searchTerms.flat(Infinity),
+        ]);
+
+        const allSentences = [];
+        allSentences.push(...documentsTerms, searchTerms);
+        const vectors = [];
+        allSentences.forEach(() => vectors.push([]))
+
+        let maxWordsNum = 0
+        words.forEach((word) => {
+            allSentences.forEach((sentenceTerms) => {
+                const includesWordNum = allSentences.filter((sentence) =>
+                    sentence.includes(word)
+                ).length;
+                if(includesWordNum > maxWordsNum){
+                    maxWordsNum = includesWordNum
+                }
+            })
+        })
+
+        words.forEach((word)=> {
+            let count = 0;
+            const denominator = getDenominator(word, allSentences);
+            allSentences.forEach((sentenceTerms) => {
+                const numerator = getNumerator(word, sentenceTerms);
+                const tf = 0.5 + 0.5 * (numerator / denominator);
+                const includesWordNum = allSentences.filter((sentence) =>
+                    sentence.includes(word)
+                ).length;
+                const idf = Math.log(maxWordsNum / includesWordNum + 1);
+                vectors[count].push(tf * idf);
+                count++;
+            })
+        })
+
+        vectors.forEach((vector, index) => {
+            const value = cosSimilarity(vectors[vectors.length - 1], vector);
+            if (value > threshold && index !== vector.length-2) {
+                result.push([documentCollections[index], value]);
+            }
+        })
+        setSearchResult(result) ;
+    }
 
     const handleDocumentChange = (evt, key) => {
         if(documentCollections[key] !== evt.target.value){
@@ -74,6 +166,14 @@ export const Lab12 = () => {
                     <div className='lab1' style={{display: 'inline'}}>
                         <b>Search query: </b>
                         <p style={{color: 'white'}}>{searchQuery}</p>
+                    </div>
+                    <div className='lab1' style={{display: 'inline'}}>
+                        <b>Search results: </b>
+                        {
+                            searchResult.map((el) => {
+                                return <p style={{color: 'white'}}>{el[0]} : {el[1]}</p>
+                            })
+                        }
                     </div>
                 </div>
             ) : <div/>}
